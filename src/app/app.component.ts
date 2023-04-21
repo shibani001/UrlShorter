6b7854d48f549dd451349f3fe546eb99a4ed5443
Shibani <shibanidas8537@gmail.com>
2023-04-21 14:44:00 +0530

    Created URL shorter in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { NgTinyUrlService } from 'ng-tiny-url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'UrlShorter';
+
+  model = {
+    inputUrl: '',
+  };
+  isFormSubmitted = false;
+  isLoading = false;
+  shortUrl = '';
+  isTextCopied = false;
+
+  constructor(private tinyUrl: NgTinyUrlService) {}
+
+  onSubmitUrlForm(urlForm: any) {
+    if (urlForm.valid) {
+      this.isLoading = true;
+      this.tinyUrl.shorten(this.model.inputUrl).subscribe(
+        (data) => {
+          this.shortUrl = data;
+          this.isFormSubmitted = true;
+          this.isLoading = false;
+        },
+        (error) => {
+          alert('Something went wrong , Please check your URL and try again');
+          this.isLoading = false;
+        }
+      );
+    }
+  }
+
+  // reset
+  reset() {
+    this.model.inputUrl = '';
+    this.isFormSubmitted = false;
+    this.isTextCopied = false;
+  }
+
+  // Copy
+
+  copyUrl(shortUrlElementRef:any){
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('type','text');
+    inputElement.setAttribute('value', shortUrlElementRef.innerHTML);
+    inputElement.select();
+    inputElement.setSelectionRange(0,999999);  // for mobile seelction
+
+    try{
+      navigator.clipboard.writeText(inputElement.value);
+      this.isTextCopied = true;
+      setTimeout(()=> {
+        this.isTextCopied = false;
+      },2000);
+    } catch(e:any){
+      console.warn('Error while copying....',e.toString())
+    }
+
+  }
 }
